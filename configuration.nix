f62888791de093116db95ffc1298d8a53ae3259e
Lumi <151774547+lumi-a@users.noreply.github.com>
2026-03-26 12:02:14 +0100

    Add flake support?

--- configuration.nix
+++ configuration.nix
@@ -9,12 +9,13 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
+ 
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos-t14"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -23,6 +24,8 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+  
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
@@ -97,16 +100,8 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.lumi = {
