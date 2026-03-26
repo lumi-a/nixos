3b1bff7f5bd5b631322680d59e7f7f0bf2f0e30b
Lumi <151774547+lumi-a@users.noreply.github.com>
2026-03-26 12:51:14 +0100

    Match system host name

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   };
 
   outputs = { self, nixpkgs }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos-t14 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
