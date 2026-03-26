79e4ec0dbd2ce5f1a56c8d11db50a20181b37f99
Lumi <151774547+lumi-a@users.noreply.github.com>
2026-03-26 20:15:46 +0100

    Improve locales

--- configuration.nix
+++ configuration.nix
@@ -41,10 +41,10 @@
     LC_MEASUREMENT = "de_DE.UTF-8";
     LC_MONETARY = "de_DE.UTF-8";
     LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
     LC_PAPER = "de_DE.UTF-8";
     LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
+    LC_TIME = "en_DK.UTF-8"; # mostly ISO compliant I hear
   };
 
   # Enable the X11 windowing system.
