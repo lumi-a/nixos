5f2d760898f9910627e5395977d1e2288c1aaf64
Lumi <151774547+lumi-a@users.noreply.github.com>
2026-03-26 12:27:07 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Lumi's NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
