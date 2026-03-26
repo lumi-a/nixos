922510f473a7294bdef4e80cc68eb360596b421c
Lumi <151774547+lumi-a@users.noreply.github.com>
2026-03-26 22:49:13 +0100

    Add locales

--- configuration.nix
+++ configuration.nix
@@ -34,6 +34,9 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
+  
+  # Identifiers from https://sourceware.org/git/?p=glibc.git;a=blob;f=localedata/SUPPORTED
+  i18n.extraLocales = [ "de_DE.UTF-8/UTF-8" "en_DK.UTF-8/UTF-8"];
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
