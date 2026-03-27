280a5ce82fbcef800e7a13d7abae92e84f01cca3
lumi <151774547+lumi-a@users.noreply.github.com>
2026-03-27 12:18:21 +0100

    Format code

--- home.nix
+++ home.nix
@@ -1,4 +1,5 @@
-{config, pkgs, ...}: {
+{ config, pkgs, ... }:
+{
   home.username = "lumi";
   home.homeDirectory = "/home/lumi";
   home.stateVersion = "25.11";
@@ -17,89 +18,87 @@
     # anything old in profile.ps1
     # yazi
     # home-managerify librewolf
-    
+
     # apps
     librewolf
     signal-desktop
     prismlauncher
   ];
-  
 
   programs = {
     home-manager.enable = true;
-    
+
     ghostty = {
-        enable = true;
-        enableFishIntegration = true;
-        installBatSyntax = true; # Not sure what this does, but can it be bad?
-        settings = {
-            font-family = "FiraCode Nerd Font";
-            font-size = "16";
-            keybind = [
-                "performable:ctrl+v=paste_from_clipboard"
-                "performable:ctrl+c=copy_to_clipboard"
-                "ctrl+t=new_tab"
-                "ctrl+w=close_surface"
-                "ctrl+n=new_window"
-            ];
-            quit-after-last-window-closed = false;
-        };
-    };
-    
-    fish = {
-        enable = true;
-        interactiveShellInit = ''
-            set fish_greeting # Disable greeting
-        '';
-        plugins = [
-            # Enable a plugin (here grc for colorized command output) from nixpkgs
-            # { name = "grc"; src = pkgs.fishPlugins.grc.src; }
+      enable = true;
+      enableFishIntegration = true;
+      installBatSyntax = true; # Not sure what this does, but can it be bad?
+      settings = {
+        font-family = "FiraCode Nerd Font";
+        font-size = "16";
+        keybind = [
+          "performable:ctrl+v=paste_from_clipboard"
+          "performable:ctrl+c=copy_to_clipboard"
+          "ctrl+t=new_tab"
+          "ctrl+w=close_surface"
+          "ctrl+n=new_window"
         ];
+        quit-after-last-window-closed = false;
+      };
     };
-    
+
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+      '';
+      plugins = [
+        # Enable a plugin (here grc for colorized command output) from nixpkgs
+        # { name = "grc"; src = pkgs.fishPlugins.grc.src; }
+      ];
+    };
+
     fzf = {
-        enable = true;
-        enableFishIntegration = true;
+      enable = true;
+      enableFishIntegration = true;
     };
-    
+
     zoxide = {
-        enable = true;
-        enableFishIntegration = true;
+      enable = true;
+      enableFishIntegration = true;
     };
-    
+
     eza = {
-        enable = true;
-        enableFishIntegration = true;
-        extraOptions = [ "--hyperlink" "--icons" "--group-directories-first" ];
+      enable = true;
+      enableFishIntegration = true;
+      extraOptions = [
+        "--hyperlink"
+        "--icons"
+        "--group-directories-first"
+      ];
     };
-    
+
     difftastic = {
-        enable = true;
-        git = {
-            enable = true;
-            diffToolMode = true;
-        };
+      enable = true;
     };
-    
+
     bat = {
-        enable = true;
-        config = {
-            style = "grid,header";
-        };
+      enable = true;
+      config = {
+        style = "grid,header";
+      };
     };
-    
+
     git = {
-        enable = true;
-        settings.user = {
-                name = "lumi";
-                email = "151774547+lumi-a@users.noreply.github.com";
-        };
+      enable = true;
+      settings.user = {
+        name = "lumi";
+        email = "151774547+lumi-a@users.noreply.github.com";
+      };
     };
-    
+
     gh = {
-        enable = true;
-        gitCredentialHelper.enable = true;
+      enable = true;
+      gitCredentialHelper.enable = true;
     };
   };
 }
-
