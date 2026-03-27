c49cb174f8c58e8d6461f2f32c4be7426e35ae09
lumi <151774547+lumi-a@users.noreply.github.com>
2026-03-27 15:34:45 +0100

    Update zed-keymap.nix

--- zed-keymap.nix
+++ zed-keymap.nix
@@ -1,38 +1,21 @@
 [
   {
-    bindings = { };
-    context = "Workspace";
-  }
-  {
-    bindings = { };
-    context = "Editor && vim_mode == insert";
-  }
-  {
     bindings = {
+      ctrl-shift-tab = "pane::ActivatePreviousItem";
       ctrl-tab = "pane::ActivateNextItem";
     };
   }
   {
     bindings = {
-      ctrl-shift-tab = "pane::ActivatePreviousItem";
-    };
-  }
-  {
-    bindings = {
+      "ctrl-g shift-up" = null;
       ctrl-p = "git::Push";
     };
     context = "GitPanel";
   }
   {
     bindings = {
-      "ctrl-g shift-up" = null;
+      ctrl-shift-g = "git_panel::Toggle";
     };
-    context = "GitPanel";
-  }
-  {
-    bindings = {
-      ctrl-shift-g = "git_panel::FocusEditor";
-    };
-    context = "!(GitPanel && (CommitEditor > Editor))";
+    context = "Workspace";
   }
 ]
