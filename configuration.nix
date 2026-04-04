d565a56284e87a1b5be50dc8e98e9374d0639476
lumi <151774547+lumi-a@users.noreply.github.com>
2026-04-04 09:15:26 +0200

    Use automatic time zone

--- configuration.nix
+++ configuration.nix
@@ -64,7 +64,7 @@
   ];
 
   # Set your time zone.
-  time.timeZone = "Europe/Berlin";
+  services.automatic-timezoned.enable = true;
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
