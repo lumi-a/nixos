8e1f29ba0e89c9f858355767f10406ae101ee655
lumi <151774547+lumi-a@users.noreply.github.com>
2026-04-06 14:50:11 +0100

    fix keyd bugs

--- keyd.nix
+++ keyd.nix
@@ -14,15 +14,14 @@
         main = {
 
           # --- Thumb modifier remaps ---
-          leftalt = "ctrl";
-          leftctrl = "alt";
-
-          # Copilot fires as Super+Shift+F23 from firmware
-          # Hold = Shift, tap = nothing
-          "leftshift+leftmeta+f23" = "overload(shift, noop)";
-
-          # AltGr = symbol layer on hold, nothing on tap
+          leftalt = "overload(control, noop)";
+          leftmeta = "overload(alt, noop)";
+          leftcontrol = "rightmeta";
           rightalt = "overload(symbols-layer, noop)";
+
+          # Copilot key, see
+          # https://github.com/rvaiya/keyd/issues/825#issuecomment-3979364570
+          "leftmeta+leftshift" = "overload(shift, noop)";
 
           # --- Top alpha row (QWERTY positions → Bone-like layout) ---
           q = "k";
@@ -35,7 +34,7 @@
           i = "l";
           o = "p";
           p = "q";
-          leftbrace = "z";
+          leftbrace = "y";
 
           # --- Home row ---
           a = "h";
@@ -51,7 +50,7 @@
           apostrophe = "v";
 
           # --- Bottom row ---
-          "102" = "j";
+          "102nd" = "j";
           z = "S-minus"; # ?
           x = "S-8"; # (
           c = "S-9"; # )
@@ -64,16 +63,16 @@
           slash = "x";
         };
 
-        "shift" = {
+        shift = {
           r = "S-5"; # %
           t = "macro(G-q)"; # @
-          y = "macro(G-rightbrace space)"; # ~
-          x = "macro(G-102)"; # |
+          z = "G-rightbrace"; # ~
+          x = "macro(G-102nd)"; # |
           c = "macro(S-equal space)"; # `
           v = "S-grave"; # °
         };
 
-        "symbols-layer" = {
+        symbols-layer = {
           # --- Top row ---
           q = "G-8"; # [
           w = "S-1"; # !
@@ -100,7 +99,7 @@
           apostrophe = "macro(grave space)"; # ^
 
           # --- Bottom row ---
-          "102" = "6";
+          "102nd" = "6";
           z = "8";
           x = "G-minus"; # \
           c = "S-backslash"; # '
@@ -108,7 +107,7 @@
           b = "noop";
           n = "S-dot"; # :
           m = "S-rightbrace"; # *
-          comma = "S-102"; # >
+          comma = "S-102nd"; # >
           dot = "9";
           slash = "7";
         };
