c33e547ab3fc61b86756c63a5435472341a03e6c
lumi <151774547+lumi-a@users.noreply.github.com>
2026-04-10 08:19:25 +0100

    Add ssh support

--- configuration.nix
+++ configuration.nix
@@ -182,8 +182,17 @@
 
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    ports = [ 5432 ];
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      AllowUsers = [ "lumi" ];
+    };
+  };
+  services.fail2ban.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
