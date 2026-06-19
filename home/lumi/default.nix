aff2c5fd054d207684ac025fa93265cede5e791d
lumi <151774547+lumi-a@users.noreply.github.com>
2026-06-20 00:36:34 +0200

    Remove deskflow

--- home/lumi/default.nix
+++ home/lumi/default.nix
@@ -32,7 +32,6 @@ in
     brave
     signal-desktop
     prismlauncher
-    deskflow
     (celestegame.override {
       # Download Celeste from itch.io and add it to the nix-store via:
       #   nix-store --add-fixed sha256 celeste-linux.zip
