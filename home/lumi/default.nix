73bfb14bcb72ba41a20671ec99fe4658cd705988
lumi <151774547+lumi-a@users.noreply.github.com>
2026-06-19 22:43:33 +0200

    Add lan-mouse

--- home/lumi/default.nix
+++ home/lumi/default.nix
@@ -32,6 +32,7 @@ in
     brave
     signal-desktop
     prismlauncher
+    lan-mouse
     (celestegame.override {
       # Download Celeste from itch.io and add it to the nix-store via:
       #   nix-store --add-fixed sha256 celeste-linux.zip
