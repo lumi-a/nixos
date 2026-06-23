e9a6f11642e0379b58540bfff0e43c4d3740cff6
lumi <151774547+lumi-a@users.noreply.github.com>
2026-06-23 13:06:28 +0200

    Use newer portal syntax

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -40,11 +40,13 @@
 
   xdg.portal = {
     enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-wlr
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-gnome
-    ];
+    extraPortals = with pkgs; [ xdg-desktop-portal-gnome ];
+    config.common = {
+      default = [ "gnome" "gtk" ];
+      "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
+      "org.freedesktop.impl.portal.RemoteDesktop" = [ "gnome" ];
+      "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+    };
   };
 
   xdg.userDirs = {
