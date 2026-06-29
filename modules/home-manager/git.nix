bff8ad92df8f321d7a5fa1cf875c25ea93b8fe4d
lumi <151774547+lumi-a@users.noreply.github.com>
2026-06-29 12:58:05 +0200

    Use SSH for signing git commits

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -8,9 +8,13 @@
           name = "lumi";
           email = "151774547+lumi-a@users.noreply.github.com";
         };
+        gpg.format = "ssh";
         init.defaultBranch = "main";
       };
-      signing.signByDefault = true;
+      signing = {
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuCXlfPiC69V6SJd6T8lypJuWkU1SuwoUuIKmh5NhrA lumi@github-bitwarden-2026-06-29";
+        signByDefault = true;
+      };
     };
 
     gh = {
